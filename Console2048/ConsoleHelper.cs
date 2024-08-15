af79b7d4ec9fb7c9dd10237197ef55a922cf5bba
Erik Morén <erik.moren78@gmail.com>
2024-08-15 16:10:57 +0200

    Cleaning up

---
+++ Console2048/ConsoleHelper.cs
@@ -0,0 +1,43 @@
+﻿namespace Console2048
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteLine(string message, ConsoleColor color = ConsoleColor.Red)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void WriteLine(this ConsoleColor color, string message)
+        {
+            WriteLine(message, color);
+        }
+
+        public static void Write(string message, ConsoleColor color = ConsoleColor.Red)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void Write(this ConsoleColor color, string message)
+        {
+            Write(message, color);
+        }
+
+        public static void WriteLine(string message, int X, int Y)
+        {
+            int oldTop = Console.CursorTop;
+            int oldLeft = Console.CursorLeft;
+
+            Console.SetCursorPosition(Y, X);
+            Console.WriteLine(message);
+
+            Console.CursorTop = oldTop;
+            Console.CursorLeft = oldLeft;
+        }
+    }
+}
